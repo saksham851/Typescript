204b610460a64f77d7c3252163719f3b423ef6c8
Saksham <121371714+saksham851@users.noreply.github.com>
2024-05-29 15:17:17 +0530

    variableme modified

--- Variableme.ts
+++ Variableme.ts
@@ -1,3 +1,12 @@
 let greeting:string="Hello saksham";
 console.log(greeting);
-// export {}
+
+//number
+let userId:number =334455;
+//Is in not to always use :number annotaion if it it too obivous.. 
+
+//boolean
+let isloggedIn:boolean=false;
+
+
+export {}
