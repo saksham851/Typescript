852b6ae78ce3783ff3aba1678093bec4bf244ef8
Saksham <121371714+saksham851@users.noreply.github.com>
2024-05-29 15:38:10 +0530

    About any type

--- Variableme.ts
+++ Variableme.ts
@@ -8,5 +8,18 @@ let userId:number =334455;
 //boolean
 let isloggedIn:boolean=false;
 
+//any
+
+//Typescript won't know which value will come in future in this variable is put as any..
+//let hero;
+
+let hero :string;
+
+function getHero(){
+    //return true; //error in compilation in typescript
+    return "ramesh"; //correctly working
+}
+
+hero=getHero();
 
 export {}
