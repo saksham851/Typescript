c705fa87579ac8ad5a2a6afd38bf0ce43004f4af
Saksham <121371714+saksham851@users.noreply.github.com>
2024-05-30 08:39:09 +0530

    Readonly and optional in ts

---
+++ myobject.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "saksham",
+    email: "sak@123",
+    isActive: true
+};
+function CreateUser(_a) {
+    var string = _a.name, boolean = _a.ispaid;
+}
+var myUser = {
+    _id: "1234",
+    name: "sak",
+    email: "s@s",
+    isActive: false
+};
+myUser.email = "sak@123";
--- myobject.ts
+++ myobject.ts
@@ -11,24 +11,56 @@ function CreateUser({name:string,ispaid:boolean}){}
 
 // Bad behaviour of object in js it will work
 
-let Newuser={name:'saksham',ispaid:false,email:"h@h.com"};
-CreateUser(Newuser);
+// let Newuser={name:'saksham',ispaid:false,email:"h@h.com"};
+// CreateUser(Newuser);
 
-function createCourse():{name:string,price:number}{
-    return {name:'React js',price:3999}
-}
+// function createCourse():{name:string,price:number}{
+//     return {name:'React js',price:3999}
+// }
 
 //Type aliases example
 
+// type User={
+//     name:string;
+//     email:string;
+//     isActive:boolean;
+// }
+
+// function CreateUsers(user:User):User{
+//     return{name:"",email:"",isActive:true}
+// }
+// CreateUsers({name:"",email:"",isActive:true});
+
+//READ ONLY AND OPTIONAL
+
 type User={
+    readonly _id:string;
     name:string;
     email:string;
-    isActive:boolean;
+    isActive:boolean
+    creditCard?:number  //optional
 }
 
-function CreateUsers(user:User):User{
-    return{name:"",email:"",isActive:true}
+let myUser:User={
+    _id:"1234",
+    name:"sak",
+    email:"s@s",
+    isActive:false
 }
-CreateUsers({name:"",email:"",isActive:true});
+
+//mixing of types
+type cardNumber={
+    cardnumber:string
+}
+
+type cardDate={
+    carddate:string
+}
+
+type cardDetails=cardNumber & cardDate  &{
+    cvv:number
+}
+
+myUser.email="sak@123";
 
 export{}
